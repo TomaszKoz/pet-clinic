abf7d014f5db27083dc10063fdfd4d64724f2270
TomaszKoz <tomasz.kozlowski@gmx.com>
2018-10-04 09:15:40 +0200

    Added Vets list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/com/archicode/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/archicode/petclinic/controllers/VetController.java
@@ -1,6 +1,9 @@
 package com.archicode.petclinic.controllers;
 
+import com.archicode.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
 }
