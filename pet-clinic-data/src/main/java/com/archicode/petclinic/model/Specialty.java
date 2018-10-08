ea86f1f639268407d6930f75fd1598c7cfba922f
TomaszKoz <tomasz.kozlowski@gmx.com>
2018-10-08 21:51:53 +0200

    Added specialities, add to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/com/archicode/petclinic/model/Specialty.java
@@ -0,0 +1,16 @@
+package com.archicode.petclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Tomasz Kozlowski (created on 08.10.2018)
+ */
+@Getter
+@Setter
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+}
+
--- pet-clinic-data/src/main/java/com/archicode/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/archicode/petclinic/model/Vet.java
@@ -1,7 +1,17 @@
 package com.archicode.petclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
 /**
  * @author Tomasz.Kozlowski (created on 27.09.2018)
  */
+@Getter
+@Setter
 public class Vet extends Person {
+
+    private Set<Specialty> specialities;
+
 }
