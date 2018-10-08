251a0c1eb5ed79d1c1359089313b8aa06ebe37b7
TomaszKoz <tomasz.kozlowski@gmx.com>
2018-10-08 22:08:48 +0200

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/archicode/petclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package com.archicode.petclinic.services;
+
+import com.archicode.petclinic.model.PetType;
+
+/**
+ * @author Tomasz Kozlowski (created on 08.10.2018)
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/archicode/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,13 @@
+package com.archicode.petclinic.services.map;
+
+import com.archicode.petclinic.model.PetType;
+import com.archicode.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Tomasz Kozlowski (created on 08.10.2018)
+ */
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+}
