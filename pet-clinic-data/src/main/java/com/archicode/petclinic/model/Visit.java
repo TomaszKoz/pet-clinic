532f738621ba0dd65d108ec3827871040bb4ccfc
tomasz.kozlowski <tomasz.kozlowski@asseco.pl>
2018-10-14 16:00:33 +0200

    Updated visit entity with JPA annotations. Closes #38

--- pet-clinic-data/src/main/java/com/archicode/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/archicode/petclinic/model/Visit.java
@@ -3,6 +3,7 @@ package com.archicode.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
@@ -10,10 +11,18 @@ import java.time.LocalDate;
  */
 @Getter
 @Setter
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
 }
