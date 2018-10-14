64825d9659fa31bf5450e22fae9ad05c20eee89e
tomasz.kozlowski <tomasz.kozlowski@asseco.pl>
2018-10-14 15:43:25 +0200

    Updated visit entity with JPA annotations. Closes #37

--- pet-clinic-data/src/main/java/com/archicode/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/archicode/petclinic/model/Speciality.java
@@ -3,13 +3,20 @@ package com.archicode.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author Tomasz Kozlowski (created on 08.10.2018)
  */
 @Getter
 @Setter
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
 }
